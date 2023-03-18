ece4171035f2c6443ceac51246b84654e2678f4c
Ryan Palmer <rpalmer9696@gmail.com>
2023-03-18 12:22:22 +0000

    Removes path resolution

--- src/graph-ql/loadAllSchema.ts
+++ src/graph-ql/loadAllSchema.ts
@@ -1,10 +1,9 @@
 import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import * as CONFIG from "./config";
-import path from "path";
 
 export async function loadAllSchema() {
-    return loadSchema(path.resolve(CONFIG.ROOT_SCHEMA_PATH), {
+    return loadSchema(CONFIG.ROOT_SCHEMA_PATH, {
         loaders: [new GraphQLFileLoader()],
     });
 }
