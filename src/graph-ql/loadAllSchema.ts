5c1704471acec6f08e9ba35c61fb2b4d77a6a891
Ryan Palmer <rpalmer9696@gmail.com>
2023-03-18 12:26:29 +0000

    Uses process path

--- src/graph-ql/loadAllSchema.ts
+++ src/graph-ql/loadAllSchema.ts
@@ -1,9 +1,10 @@
 import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import * as CONFIG from "./config";
+import path from "path";
 
 export async function loadAllSchema() {
-    return loadSchema(CONFIG.ROOT_SCHEMA_PATH, {
+    return loadSchema(path.join(process.cwd(), CONFIG.ROOT_SCHEMA_PATH), {
         loaders: [new GraphQLFileLoader()],
     });
 }
